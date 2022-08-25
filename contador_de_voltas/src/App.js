ac296003b29fc83078b462687754d44c9f36d772
Moisés R. Martins <90099815+moisesrmartins@users.noreply.github.com>
2022-08-25 11:12:07 -0300

    React_2_Contador_Voltas Parte 2: Alterações e inicio do Desenvolvimento

--- contador_de_voltas/src/App.js
+++ contador_de_voltas/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>
+        10
+        <br/>
+        Turns
+      </p>
+      <button>+</button>
+      <button>-</button>
+      <p>
+        01:30
+        <br/>
+        Average time per lap
+      </p>
+      <button>Start</button>
+      <button>Restart</button>
     </div>
   );
 }
