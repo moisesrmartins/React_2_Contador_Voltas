23575f1b00e83c0e0b89b41c6608b1463a3f69da
Moisés R. Martins <90099815+moisesrmartins@users.noreply.github.com>
2022-08-25 11:15:39 -0300

    React_2_Contador_Voltas Parte 4: Adição do ShowTime

--- contador_de_voltas/src/App.js
+++ contador_de_voltas/src/App.js
@@ -10,17 +10,23 @@ const ShowLaps = (props) => {
   );
 };
 
+const ShowTime = (props) => {
+  return (
+    <p>
+      {props.time}
+      <br/>
+      Average time per lap
+    </p>
+  );
+};
+
 function App() {
   return (
     <div className="App">
-      <ShowLaps laps="10"/>
+      <ShowLaps laps = "10"/>
       <button>+</button>
       <button>-</button>
-      <p>
-        01:30
-        <br/>
-        Average time per lap
-      </p>
+      <ShowTime time = "01:30"/>
       <button>Start</button>
       <button>Restart</button>
     </div>
